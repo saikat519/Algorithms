0943c35dbaeccce2f682c572b42a78ff007b2f49
Saikat Mukhopadhyay <49401892+saikat519@users.noreply.github.com>
2020-06-10 20:52:55 +0530

    Add files via upload

---
+++ Flood_Fill_Recursion.py
@@ -0,0 +1,25 @@
+def floodfill(arr,r,c,oldC,newC):
+       row =len(arr)-1
+       col =len(arr[0])-1
+       if r>row or r<0 or c>col or c<0:
+              return
+       if arr[r][c] != oldC:
+              return
+       arr[r][c]=newC
+       floodfill(arr,r+1,c,oldC,newC)
+       floodfill(arr,r-1,c,oldC,newC)
+       floodfill(arr,r,c+1,oldC,newC)
+       floodfill(arr,r,c-1,oldC,newC)
+
+arr = [[1, 1, 1, 0, 1, 1, 1, 1],
+       [1, 1, 0, 1, 1, 1, 0, 0],
+       [1, 0, 0, 1, 1, 0, 1, 1],
+       [1, 2, 2, 2, 2, 0, 1, 0],
+       [1, 1, 1, 2, 2, 0, 1, 0],
+       [1, 1, 1, 2, 2, 2, 2, 0],
+       [1, 1, 1, 0, 1, 2, 1, 1],
+       [1, 1, 0, 1, 1, 2, 2, 1],
+       [1, 0, 1, 1, 1, 2, 2, 1]]
+floodfill(arr,3,0,1,4)
+for i in range(len(arr)):
+       print(arr[i])
